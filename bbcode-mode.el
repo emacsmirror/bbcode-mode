8a28ee9fe2d230c33b9c3fba19b07cf32e6a9b1a
Lassi Kortela <lassi@lassi.io>
2018-03-19 12:19:39 +0200

    Fix font-lock regexp for multi-attribute tags
    Two formats for an opening tag:
    
    1. [tag=value]
    2. [tag attr1=value1 attr2=value2 ...]

--- bbcode-mode.el
+++ bbcode-mode.el
@@ -86,7 +86,7 @@
     ;; Opening tag with attribute.
     (,(concat (regexp-quote "[")
               (regexp-opt (mapcar #'car bbcode-tags) t)
-              (concat "=" "\"?" "\\(.*?\\)" "\"?")
+              "[ =]\\(.*?\\)"
               (regexp-quote "]"))
      (0 font-lock-keyword-face)
      (2 font-lock-preprocessor-face t))
