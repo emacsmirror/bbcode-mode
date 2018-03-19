3248745960278fe34598cc29bdc9965482cc7f3f
Lassi Kortela <lassi@lassi.io>
2018-03-19 13:06:50 +0200

    Add `eval-and-compile` around `defconst`
    To ensure that definitions are available during `byte-compile-file`
    even if the file has not been loaded.

--- bbcode-mode.el
+++ bbcode-mode.el
@@ -49,56 +49,58 @@
 ;; Keys for creating lists begin with 'C-c C-l'.
 ;; Keys for tables begin with 'C-c C-b'
 ;; Keys for special, uncommon tags begin with 'C-c C-s'.
-(defconst bbcode-tags
-  '(("*"           font-lock-keyword-face        "C-c C-l *" nil)
-    ("attachment"  font-lock-variable-face       "C-c C-s a" 1)
-    ("b"           bold                          "C-c C-t b" 1)
-    ("center"      font-lock-keyword-face        "C-c C-t n" 1)
-    ("code"        font-lock-function-name-face  "C-c C-t c" t)
-    ("color"       font-lock-variable-name-face  "C-c C-f c" 1 color)
-    ("del"         default                       "C-c C-t d" 1)
-    ("email"       link                          "C-c C-t e" 1)
-    ("font"        font-lock-variable-name-face  "C-c C-f f" 1)
-    ("gvideo"      font-lock-variable-name-face  "C-c C-s g" 1)
-    ("i"           italic                        "C-c C-t i" 1)
-    ("img"         link                          "C-c C-t m" 1 width height)
-    ("li"          font-lock-keyword-face        "C-c C-l i" 1)
-    ("list"        font-lock-keyword-face        "C-c C-l l" t)
-    ("manual"      font-lock-variable-name-face  "C-c C-s m" 1)
-    ("ol"          font-lock-keyword-face        "C-c C-l o" t)
-    ("quote"       font-lock-doc-face            "C-c C-t q" t name)
-    ("s"           default                       "C-c C-t s" 1)
-    ("size"        font-lock-variable-name-face  "C-c C-f s" 1 size)
-    ("style"       default                       "C-C C-f y" 1 color size)
-    ("table"       font-lock-keyword-face        "C-c C-b t" t)
-    ("td"          font-lock-variable-name-face  "C-c C-b d" 1)
-    ("th"          bold                          "C-c C-b h" 1)
-    ("tr"          font-lock-keyword-face        "C-c C-b r" 1)
-    ("u"           underline                     "C-c C-t u" 1)
-    ("ul"          font-lock-keyword-face        "C-c C-l u" t)
-    ("url"         link                          "C-c C-t l" 1 url)
-    ("wiki"        font-lock-variable-name-face  "C-c C-s w" 1)
-    ("youtube"     font-lock-variable-name-face  "C-c C-s y" 1)))
+(eval-and-compile
+  (defconst bbcode-tags
+    '(("*"           font-lock-keyword-face        "C-c C-l *" nil)
+      ("attachment"  font-lock-variable-face       "C-c C-s a" 1)
+      ("b"           bold                          "C-c C-t b" 1)
+      ("center"      font-lock-keyword-face        "C-c C-t n" 1)
+      ("code"        font-lock-function-name-face  "C-c C-t c" t)
+      ("color"       font-lock-variable-name-face  "C-c C-f c" 1 color)
+      ("del"         default                       "C-c C-t d" 1)
+      ("email"       link                          "C-c C-t e" 1)
+      ("font"        font-lock-variable-name-face  "C-c C-f f" 1)
+      ("gvideo"      font-lock-variable-name-face  "C-c C-s g" 1)
+      ("i"           italic                        "C-c C-t i" 1)
+      ("img"         link                          "C-c C-t m" 1 width height)
+      ("li"          font-lock-keyword-face        "C-c C-l i" 1)
+      ("list"        font-lock-keyword-face        "C-c C-l l" t)
+      ("manual"      font-lock-variable-name-face  "C-c C-s m" 1)
+      ("ol"          font-lock-keyword-face        "C-c C-l o" t)
+      ("quote"       font-lock-doc-face            "C-c C-t q" t name)
+      ("s"           default                       "C-c C-t s" 1)
+      ("size"        font-lock-variable-name-face  "C-c C-f s" 1 size)
+      ("style"       default                       "C-C C-f y" 1 color size)
+      ("table"       font-lock-keyword-face        "C-c C-b t" t)
+      ("td"          font-lock-variable-name-face  "C-c C-b d" 1)
+      ("th"          bold                          "C-c C-b h" 1)
+      ("tr"          font-lock-keyword-face        "C-c C-b r" 1)
+      ("u"           underline                     "C-c C-t u" 1)
+      ("ul"          font-lock-keyword-face        "C-c C-l u" t)
+      ("url"         link                          "C-c C-t l" 1 url)
+      ("wiki"        font-lock-variable-name-face  "C-c C-s w" 1)
+      ("youtube"     font-lock-variable-name-face  "C-c C-s y" 1))))
 
-(defconst bbcode-font-lock-keywords
-  `(;; Opening tag.
-    (,(concat (regexp-quote "[")
-              (regexp-opt (mapcar #'car bbcode-tags) t)
-              (regexp-quote "]"))
-     (0 font-lock-keyword-face))
-    ;; Opening tag with attribute.
-    (,(concat (regexp-quote "[")
-              (regexp-opt (mapcar #'car bbcode-tags) t)
-              "[ =]\\(.*?\\)"
-              (regexp-quote "]"))
-     (0 font-lock-keyword-face)
-     (2 font-lock-preprocessor-face t))
-    ;; Closing tag.
-    (,(concat (regexp-quote "[/")
-              (regexp-opt (mapcar #'car bbcode-tags) t)
-              (regexp-quote "]"))
-     (0 font-lock-keyword-face)))
-  "Regular expressions to highlight BBCode markup.")
+(eval-and-compile
+  (defconst bbcode-font-lock-keywords
+    `(;; Opening tag.
+      (,(concat (regexp-quote "[")
+                (regexp-opt (mapcar #'car bbcode-tags) t)
+                (regexp-quote "]"))
+       (0 font-lock-keyword-face))
+      ;; Opening tag with attribute.
+      (,(concat (regexp-quote "[")
+                (regexp-opt (mapcar #'car bbcode-tags) t)
+                "[ =]\\(.*?\\)"
+                (regexp-quote "]"))
+       (0 font-lock-keyword-face)
+       (2 font-lock-preprocessor-face t))
+      ;; Closing tag.
+      (,(concat (regexp-quote "[/")
+                (regexp-opt (mapcar #'car bbcode-tags) t)
+                (regexp-quote "]"))
+       (0 font-lock-keyword-face)))
+    "Regular expressions to highlight BBCode markup."))
 
 (defun bbcode-quote-attribute-value (value)
   "Put quotes around BBCode tag attribute VALUE if needed."
